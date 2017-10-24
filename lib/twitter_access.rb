37187ad4bbe009d2799b5cbe7d3e8734ccf92174
Scott Pierce <scottpiercew@gmail.com>
2017-10-23 21:38:14 -0400

    class OrgTweets

--- lib/twitter_access.rb
+++ lib/twitter_access.rb
@@ -2,21 +2,21 @@
 # index method of pages controller open .yml file
 
 
-class Org_tweets
+class OrgTweets
 
   organizations_twitter_handle = ["TeamRubicon", "TeamRWB"]
   tweets = []
   all_tweets = []
 
   attr_accessor :organizations_twitter_handle, :all_tweets
-  def load
+
+  def access_tweets
     client = Twitter::REST::Client.new do |config|
     config.consumer_key        = Secret.get(‘TWITTER_CONSUMER_KEY’)
     config.consumer_secret     = Secret.get(‘TWITTER_CONSUMER_SECRET’)
     config.access_token        = Secret.get(‘TWITTER_ACCESS_TOKEN’)
     config.access_token_secret = Secret.get(‘TWITTER_ACCESS_SECRET’)
     end
-  end
 
     @organizations_twitter_handle.each do |grab_tweets|
       tweets = client.user_timeline(grab_tweets, count: 3)
@@ -24,5 +24,4 @@ class Org_tweets
     end
     File.write("tweets.yml", YAML.dump(@all_tweets))
   end
-
 end
