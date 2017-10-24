7d4f2c16c715716ed049a694cb1664347fa84a03
Scott Pierce <scottpiercew@gmail.com>
2017-10-23 20:46:35 -0400

    adding File.write

--- config/initializers/veteran_nexus.rb
+++ config/initializers/veteran_nexus.rb
@@ -1,3 +1,3 @@
-# # Rails.configuration.x.env_using_secrets
-# Veterans_nexus::Application.config.x.envs_that_use_secrets_yml = %w(development test)
-# Veterans_nexus::Application.config.x.envs_that_use_env = %w(staging production)
+# Rails.configuration.x.env_using_secrets
+VeteranNexus::Application.config.x.envs_that_use_secrets_yml = %w(development test)
+VeteranNexus::Application.config.x.envs_that_use_env = %w(staging production)
--- lib/twitter_access.rb
+++ lib/twitter_access.rb
@@ -1,25 +1,28 @@
 #controller to read .yml
 # index method of pages controller open .yml file
 
-# 
-# class Org_tweets
-#
-# organizations = ["TeamRubicon", "TeamRWB"]
-# tweets = []
-#
-#   def self.get
-#     client = Twitter::REST::Client.new do |config|
-#     config.consumer_key        = Secret.get(‘TWITTER_CONSUMER_KEY’)
-#     config.consumer_secret     = Secret.get(‘TWITTER_CONSUMER_SECRET’)
-#     config.access_token        = Secret.get(‘TWITTER_ACCESS_TOKEN’)
-#     config.access_token_secret = Secret.get(‘TWITTER_ACCESS_SECRET’)
-#     end
-#   end
-#
-#   organizations.each do |grab_tweets|
-#   tweets = client.user_timeline(grab_tweets, count: 1)
-#   tweets.each { |tweet| puts tweet.full_text }
-#   end
-#
-#
-# end
+
+class Org_tweets
+
+  organizations_twitter_handle = ["TeamRubicon", "TeamRWB"]
+  tweets = []
+  all_tweets = []
+
+  attr_accessor :organizations_twitter_handle, :all_tweets
+  def load
+    client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = Secret.get(‘TWITTER_CONSUMER_KEY’)
+    config.consumer_secret     = Secret.get(‘TWITTER_CONSUMER_SECRET’)
+    config.access_token        = Secret.get(‘TWITTER_ACCESS_TOKEN’)
+    config.access_token_secret = Secret.get(‘TWITTER_ACCESS_SECRET’)
+    end
+  end
+
+    @organizations_twitter_handle.each do |grab_tweets|
+      tweets = client.user_timeline(grab_tweets, count: 3)
+      @all_tweets << tweets
+    end
+    File.write("tweets.yml", YAML.dump(@all_tweets))
+  end
+
+end
